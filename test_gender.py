9b0824a0ec319287866297b460289fbe69123aa1
Abhijeet Kumar <abhijeet.charlie@gmail.com>
2017-06-13 17:57:10 +0530

    Add files via upload

---
+++ test_gender.py
@@ -0,0 +1,49 @@
+#test_gender.py
+import os
+import cPickle
+import numpy as np
+from scipy.io.wavfile import read
+import python_speech_features as mfcc
+from sklearn import preprocessing
+import warnings
+warnings.filterwarnings("ignore")
+
+def get_MFCC(sr,audio):
+    features = mfcc.mfcc(audio,sr, 0.025, 0.01, 13,appendEnergy = False)
+    feat     = np.asarray(())
+    for i in range(features.shape[0]):
+        temp = features[i,:]
+        if np.isnan(np.min(temp)):
+            continue
+        else:
+            if feat.size == 0:
+                feat = temp
+            else:
+                feat = np.vstack((feat, temp))
+    features = feat;
+    features = preprocessing.scale(features)
+    return features
+
+sourcepath = "D:\\Audioset-Google\\female_clips\\"          #path to testing data
+modelpath  = "D:\\gender_models\\"      #path to saved models
+
+gmm_files = [os.path.join(modelpath,fname) for fname in 
+              os.listdir(modelpath) if fname.endswith('.gmm')]
+models    = [cPickle.load(open(fname,'r')) for fname in gmm_files]
+genders   = [fname.split("\\")[-1].split(".gmm")[0] for fname 
+              in gmm_files]
+files     = [os.path.join(sourcepath,f) for f in os.listdir(sourcepath) 
+              if f.endswith(".wav")] 
+
+for f in files:
+    print f.split("\\")[-1]
+    sr, audio  = read(f)
+    features   = get_MFCC(sr,audio)
+    scores     = None
+    log_likelihood = np.zeros(len(models)) 
+    for i in range(len(models)):
+        gmm    = models[i]         #checking with each model one by one
+        scores = np.array(gmm.score(features))
+        log_likelihood[i] = scores.sum()
+    winner = np.argmax(log_likelihood)
+    print "\tdetected as - ", genders[winner],"\n\tscores:female ",log_likelihood[0],",male ", log_likelihood[1],"\n"
---
+++ train_models.py
@@ -0,0 +1,39 @@
+#train_models.py
+
+#train_models.py
+
+import os
+import cPickle
+import numpy as np
+from scipy.io.wavfile import read
+from sklearn.mixture import GMM 
+import python_speech_features as mfcc
+from sklearn import preprocessing
+
+def get_MFCC(sr,audio):
+    features = mfcc.mfcc(audio,sr, 0.025, 0.01, 13,appendEnergy = False)
+    features = preprocessing.scale(features)
+    return features
+
+source   = "D:\\training_data\\male\\"   #path to training data
+dest     = "D:\\gender_models\\"         #path to save trained model
+files    = [os.path.join(source,f) for f in os.listdir(source) if 
+             f.endswith('.wav')] 
+features = np.asarray(());
+
+for f in files:
+    sr,audio = read(f)
+    vector   = get_MFCC(sr,audio)
+    if features.size == 0:
+        features = vector
+    else:
+        features = np.vstack((features, vector))
+
+gmm = GMM(n_components = 8, n_iter = 200, covariance_type='diag',
+        n_init = 3)
+gmm.fit(features)
+picklefile = f.split("\\")[-2].split(".wav")[0]+".gmm"
+
+# model saved as male.gmm
+cPickle.dump(gmm,open(dest + picklefile,'w'))
+print 'modeling completed for gender:',picklefile
