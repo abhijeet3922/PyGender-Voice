29825462f5adad7213d81e455e10a1de69ed1ffc
Abhijeet Kumar <abhijeet.charlie@gmail.com>
2017-06-13 18:37:40 +0530

    Create train_models.py

--- train_models.py
+++ train_models.py
@@ -7,6 +7,8 @@ from scipy.io.wavfile import read
 from sklearn.mixture import GMM 
 import python_speech_features as mfcc
 from sklearn import preprocessing
+import warnings
+warnings.filterwarnings("ignore")
 
 def get_MFCC(sr,audio):
     features = mfcc.mfcc(audio,sr, 0.025, 0.01, 13,appendEnergy = False)
